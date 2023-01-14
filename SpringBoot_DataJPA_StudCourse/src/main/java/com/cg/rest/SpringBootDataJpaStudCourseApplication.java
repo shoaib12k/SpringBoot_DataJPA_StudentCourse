141418ada8c9becbce35acc14429a7762bc273fb
shoaib12k <shoaib1207k@gmail.com>
2023-01-14 10:32:24 +0530

    Created spring project

---
+++ SpringBoot_DataJPA_StudCourse/src/main/java/com/cg/rest/SpringBootDataJpaStudCourseApplication.java
@@ -0,0 +1,13 @@
+package com.cg.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaStudCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaStudCourseApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_DataJPA_StudCourse/src/test/java/com/cg/rest/SpringBootDataJpaStudCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaStudCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
